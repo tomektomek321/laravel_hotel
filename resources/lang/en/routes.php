5d790823c2ce8a03244f4c78d650b2162bc1b8bb
Tomek <tomek1989gw@wppl>
2021-10-01 01:53:35 +0200

    add pl lang

---
+++ resources/lang/en/routes.php
@@ -0,0 +1,23 @@
+<?php
+/*
+|--------------------------------------------------------------------------
+| resources/lang/en/routes.php *** Copyright netprogs.pl | available only at Udemy.com | further distribution is prohibited  ***
+|--------------------------------------------------------------------------
+*/
+
+/* Lecture 6 */
+return [
+    
+    
+    'article' => 'article',
+    'object' => 'object',
+    'person' => 'person',
+    'room' => 'room',
+    'roomsearch' => 'roomsearch',
+    'myobjects' => 'myobjects',
+    'saveobject' => 'saveobject',
+    'profile' => 'profile',
+    'saveroom' => 'saveroom',
+    
+    
+];
