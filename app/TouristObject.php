4538e396355b06f2478ddf58442477e4a3157678
Tomek <tomek1989gw@wppl>
2021-10-02 17:20:52 +0200

    City and Photo models and relations to TouristObject

--- app/TouristObject.php
+++ app/TouristObject.php
@@ -7,4 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class TouristObject extends Model
 {
     protected $table = 'objects';
+
+    public function city()
+    {
+        return $this->belongsTo('App\City');
+    }
+
+    public function photos()
+    {
+        return $this->morphMany('App\Photo', 'photoable');
+    }
 }
