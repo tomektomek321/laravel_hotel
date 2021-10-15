d533cb812093476d3cb396ce1ab2e5e021e3250f
Tomek <tomek1989gw@wp.pl>
2021-10-15 23:47:26 +0200

    BackendRepository Interface

---
+++ app/Enjoythetrip/Repositories/BackendRepository.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Enjoythetrip\Repositories;
+
+use App\Enjoythetrip\Interfaces\BackendRepositoryInterface;
+
+class BackendRepository implements BackendRepositoryInterface {
+
+}
+
+
