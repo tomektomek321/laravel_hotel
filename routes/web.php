c79f2a7ffe06a8adedd4e763768080bc7da328f6
Tomek <tomek1989gw@wp.pl>
2022-07-26 17:39:05 +0200

    update auth

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,6 @@ Route::get('/', function () {
 });
 
 Route::get('/',[App\Http\Controllers\FrontendController::class, 'index']);
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
